f30338ddebcc72dc8fc840b1a77bd66ac6db06f5
igorFrotte <igorfrotte@gmail.com>
2022-09-10 18:45:57 -0300

    Transictions Ready

--- index.js
+++ index.js
@@ -31,6 +31,12 @@ const signInSchema = joi.object({
   password: joi.string().required()
 }); 
 
+const transactionSchema = joi.object({
+  type: joi.string().required().valid('positive','negative'),
+  value: joi.number().positive().precision(2).required(),
+  description: joi.string().required()
+}); 
+
 setInterval( async () => {
   const realTime = Date.now();
   const HOURS_2 = 1000 * 60 * 60 * 2;
@@ -110,7 +116,56 @@ app.post("/sign-in", async (req, res) => {
       creatTime: Date.now()
     })
 
-    return res.status(200).send(token);
+    return res.status(200).send({token, user: user.name});
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+});
+
+app.get("/transactions", async (req, res) => {
+  const token = req.headers.authorization?.replace('Bearer ', '');
+
+  try {
+    const session = await db.collection('token').findOne({ token });
+    if (!session) {
+      return res.send(401);
+    }
+
+    const transactions = await db.collection('transaction').find({ userId: session.userId }).toArray();
+
+    return res.status(200).send(transactions);
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+});
+
+app.post("/transactions", async (req, res) => {
+  const token = req.headers.authorization?.replace('Bearer ', '');
+
+  const validation = transactionSchema.validate(req.body, { abortEarly: false });
+  if (validation.error) {
+    const erros = validation.error.details.map((detail) => detail.message);
+    return res.status(422).send(erros);
+  }
+
+  const value = Number(req.body.value).toFixed(2);
+
+  try {
+    const session = await db.collection('token').findOne({ token });
+    if (!session) {
+      return res.send(401);
+    }
+
+    const transactions = await db
+      .collection('transaction')
+      .insertOne({ 
+        ...req.body,
+        value,
+        userId: session.userId,
+        date: dayjs(new Date()).format('DD/MM')
+      });
+
+    return res.status(200).send(transactions);
   } catch (error) {
     return res.status(500).send(error.message);
   }
